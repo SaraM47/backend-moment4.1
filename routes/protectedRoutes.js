7f27d5f7499666bb7a91c8885ef6ac0e7b1c3af0
SaraMsoso <Sara.soder44@gmail.com>
2025-05-24 16:36:08 +0200

    Implemented protected route for favorite movies

--- routes/protectedRoutes.js
+++ routes/protectedRoutes.js
@@ -10,4 +10,13 @@ router.get('/', verifyToken, (req, res) => {
   res.json({ message: 'Du är inloggad och har åtkomst till skyddad data.' });
 });
 
+// Protected route: Return static favorite movies
+router.get('/movies', verifyToken, (req, res) => {
+  res.json([
+    { title: "Zootopia", genre: "Animated" },
+    { title: "The Kissing Booth", genre: "Romantic" },
+    { title: "Rush Hour", genre: "Action" }
+  ]);
+});
+
 module.exports = router;
